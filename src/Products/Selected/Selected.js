59af99dc2763410c16f02d7879d18f8596ec6aef
RajuSultan <rajusultan12@gmail.com>
2022-03-26 19:59:26 +0600

    7th

--- src/Products/Selected/Selected.js
+++ src/Products/Selected/Selected.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
 import './Selected.css'
 
 const Selected = (props) => {
     let items = props.choosen;
     let newItems = [...items];
     const [bestOne, setBestOne] = useState([]);
+
     function random(items) {
         if (items.length === 0) {
             alert("You don't have seleted item, add something");
@@ -15,14 +18,19 @@ const Selected = (props) => {
             document.getElementById("dis-1").style.display = "block";
 
         }
-    }
+    };
+    function deleteItem(data) {
+        newItems = newItems.filter(person => person.name != data.name);
+        document.getElementById(data.id).style.display = "none";
+    };
+
     return (
         <div className='main'>
             <h1>Selected Items</h1>
             <hr />
             <div id='dis'>
                 {
-                    newItems.map(item => <p>{item.name}</p>)
+                    newItems.map(item => <p id={item.id}>{item.name} <span onClick={() => deleteItem(item)} ><FontAwesomeIcon icon={faDeleteLeft}></FontAwesomeIcon></span></p>)
                 }
             </div>
             <button className='btn-1' onClick={() => random(newItems)} >Choose 1 For Me</button><br />
